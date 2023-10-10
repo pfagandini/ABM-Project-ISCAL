2476eb5610312c0e4a79db63e6cfb9514cdb27bb
pfagandi <fagandini@protonmail.com>
2023-10-10 14:43:08 +0100

    list for export to excel begining

--- run.py
+++ run.py
@@ -1,23 +1,29 @@
 from model import model
 import matplotlib.pyplot as plt
+import xlsxwriter
 
-samples = 100
-agents = 500
-steps = 50
+workbook = xlsxwriter.Workbook('Data.xlsx')
+worksheet_wealth = workbook.add_worksheet('wealth')
+worksheet_political = workbook.add_worksheet('political')
+worksheet_moral = workbook.add_worksheet('moral')
+worksheet_consumption = workbook.add_worksheet('consumption')
+
+samples = 1
+agents = 10
+steps = 5
+
+wealth_list = []
+political_view_list = []
+moral_behavior_list = []
+consumption_list = []
 
 wealth = []
 political_view = []
 moral_behavior = []
 consumption = []
-tax = []
-
-av_wealth = []
-av_polit = []
 
 for j in range(samples):
     mymodel = model(agents)
-
-    av_wealth_aux = []
 
     for a in mymodel.schedule.agents:
         wealth.append(a.wealth)
@@ -25,9 +31,24 @@ for j in range(samples):
         moral_behavior.append(a.moral_behavior)
         consumption.append(a.consumed)
         
+    wealth_list.append(wealth)
+    political_view_list.append(political_view)
+    moral_behavior_list.append(moral_behavior)
+    consumption_list.append(consumption)    
+        
+    wealth = []
+    political_view = []
+    moral_behavior = []
+    consumption = []
+
     for i in range(steps):
         print(f'run {j} step {i}')
         mymodel.step()
+
+        wealth = []
+        political_view = []
+        moral_behavior = []
+        consumption = []
 
         for a in mymodel.schedule.agents:
             wealth.append(a.wealth)
@@ -35,17 +56,38 @@ for j in range(samples):
             moral_behavior.append(a.moral_behavior)
             consumption.append(a.consumed)
 
-        av_wealth_aux.append(mymodel.av_wealth)
-    
-    av_wealth.append(av_wealth_aux)
+        wealth_list.append(wealth)
+        political_view_list.append(political_view)
+        moral_behavior_list.append(moral_behavior)
+        consumption_list.append(consumption)    
+        
+###############
+# Data Export #
+###############
 
-av_wealth = [sum(sub_list) / len(sub_list) for sub_list in zip(*av_wealth)]
-    
-#################
-# Data Analysis #
-#################
+def trans(M):
+    return [[M[j][i] for j in range(len(M))] for i in range(len(M[0]))]
 
-ax = plt.plot(av_wealth)
-plt.xlabel('Period (t)')
-plt.ylabel('Av. Wealth')
-plt.show()
+wealth_list = trans(wealth_list)
+
+ag = []
+for i in range(0, agents):
+    ag.append(str(f"Agent {i+1}"))
+print(ag)
+
+pe = []
+for i in range(0, steps):
+    pe.append(str(f"Period {i+1}"))
+print(pe)
+
+head = ['periods \ agents'] + ag
+
+# Write the data.
+worksheet_wealth.write_row(0 , 0 , head)
+worksheet_wealth.write_row(1 , 0 , pe)
+
+for i in range(0, len(ag)):
+    print(i)
+    worksheet_wealth.write_row(1 , i + 1 , wealth_list[i])
+
+workbook.close()
